b7585c7885234c60a64d8eff8e25aa39b92229a1
James <ugwuayijameson@gmail.com>
2023-03-17 09:35:51 +0100

    print sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - the main block function
- * 
+ *
  * Return: return empty string
  */
 int main(void)
@@ -16,11 +16,11 @@ int main(void)
 	long long int l;
 	float m;
 
-	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(i) );
-	printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(j) );
-	printf ("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(k) );
-	printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(l) );
-	printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(m)  );
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(j));
+	printf ("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+	printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(l));
+	printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(m));
 
 	return (0);
 }

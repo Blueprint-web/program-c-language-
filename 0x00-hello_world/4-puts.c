372cc93c829df7539eae4b599aa59e77c5a6754f
James <ugwuayijameson@gmail.com>
2023-03-17 02:59:40 +0100

    c files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,22 @@
 
 /**
  * initializing a string and printing it out without using printf
+ * @str: inputs the string 
+ * @printf: prints nothing
+ * return: value will be zero
  */
 int main(void)
 {
+	/**
+	 * this is the string
+	 */
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	/**
+	 * this is the print out
+	 * using put
+	 */
 	puts(str);
-	fprintf(stderr, "erro\n");
+	fprintf(stderr, " ");
 	return(0);
 }

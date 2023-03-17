81a624696b02868b4dd4c31123f7df3f5a6b2ac4
James <ugwuayijameson@gmail.com>
2023-03-17 03:25:53 +0100

    c project

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-/* this is the main */
+/**
+ *this is the main
+ */
 int main(void)
 {
-	/* this is the string */
+	/**
+	 * this is the string 
+	 */
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 	stderr;
 
-	/* after printing retun */
-	return(0);
+	/**
+	 * after printing retun 
+	 */
+	return (0);
 }

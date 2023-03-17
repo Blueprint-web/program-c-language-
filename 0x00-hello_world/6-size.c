08e8e01df5b2ce9069f88511a248292a28f1629b
James <ugwuayijameson@gmail.com>
2023-03-17 09:27:50 +0100

    print sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - the main block function
+ * 
+ * Return: return empty string
+ */
+int main(void)
+{
+	/**
+	 * @i,j,k,l,m: variabe name identifier
+	 */
+	char i;
+	int j;
+	long int k;
+	long long int l;
+	float m;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(i) );
+	printf("size of an int: %lu byte(s)\n",(unsigned long)sizeof(j) );
+	printf ("size of a long int: %lu byte(s)\n",(unsigned long)sizeof(k) );
+	printf("size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(l) );
+	printf("size of a float: %lu byte(s)\n",(unsigned long)sizeof(m)  );
+
+	return (0);
+}

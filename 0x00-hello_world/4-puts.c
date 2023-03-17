11786ec79bc48c7408661cf2bf235c5dc322777e
James <ugwuayijameson@gmail.com>
2023-03-17 01:34:30 +0100

    first c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+	/**
+	 * my string initialization
+	 */
+	char mystr[] = "programing is like building a multilingual puzzle";
+
+	/** 
+	 * writting out the string to stand out
+	 */
+
+	puts(mystr); 
+
+	return (0);
+}
+	

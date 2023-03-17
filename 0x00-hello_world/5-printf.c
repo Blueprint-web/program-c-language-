2a7df9bf056212644af8b071dce7052ad6a7e07d
James <ugwuayijameson@gmail.com>
2023-03-17 04:18:30 +0100

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - for holding the code
+ * Return: return no value
+ */
+int main(void)
+{
+	/**
+	 * print - to print out the function
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	stderr;
+
+	return (0);
+}

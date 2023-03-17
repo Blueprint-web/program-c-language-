f0aa7a0994a9ee06f168bddcf6720b574eb5700b
James <ugwuayijameson@gmail.com>
2023-03-17 01:50:07 +0100

    my first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int main ()
+/**
+ * initializing a string and printing it out without using printf
+ */
+int main(void)
 {
 	/**
 	 * my string initialization
 	 */
-	char mystr[] = "programing is like building a multilingual puzzle";
-
+	char mystr[] = "\"programming is like building a multilingual puzzle";
 	/** 
 	 * writting out the string to stand out
 	 */
-
 	puts(mystr); 
 
-	return (0);
+	return(0);
 }
-	

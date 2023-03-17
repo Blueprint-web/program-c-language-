7957e47da1397f97fe06404f25bbf8c768b3e982
James <ugwuayijameson@gmail.com>
2023-03-17 03:56:26 +0100

    my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,22 +4,16 @@
  * main - this is the main holding my code
  * @str: input the display string
  *
- * discription: this function is meant to print out a quotation string using the put function.
+ * discription: this is a print funtion using put
  * header: no heaader
  * Return: after initialization return an empty string
  */
 int main(void)
 {
-	/**
-	 * @str: this is the string
-	 */
 	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 	stderr;
 
-	/**
-	 * Return: after printing retun
-	 */
 	return (0);
 }

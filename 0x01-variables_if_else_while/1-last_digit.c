493a6b6672d0193bf156a5beb2a8a9dbce4bfd6b
James <ugwuayijameson@gmail.com>
2023-03-18 14:55:55 +0100

    last degit comited

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,11 +12,11 @@
 int main(void)
 {
 	int n;
+	int o = n % 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int o = n % 10;
 	if (o > 5)
 		printf("last degit of %d is %d and is greater than 5\n", n, o);
 	else if(o == 0)

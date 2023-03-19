faab34e84a6d193cf76abf092af7c0c90a3afad8
Egba James <ugwuayijameson@gmail.com>
2023-03-19 15:19:42 -0700

    comco

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - this is the main function.
+ *
+ * Return: always return 0.
+ */
+int main(void)
+{
+	int i;
+	int j = ',';
+	int l = ' ';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(j);
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}

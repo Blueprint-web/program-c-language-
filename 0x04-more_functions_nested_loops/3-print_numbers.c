0c1cf9a345f7164f8ff0be93517a6f770e14da6f
Egba James <ugwuayijameson@gmail.com>
2023-03-23 15:56:22 -0700

    printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - this function prints out numbers.
+ *
+ * Return: return no value.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}

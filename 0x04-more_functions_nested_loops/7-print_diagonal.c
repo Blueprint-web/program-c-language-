78f76c037caf6af9e64036997dc771d7afe82667
Egba James <ugwuayijameson@gmail.com>
2023-03-24 14:26:38 -0700

    diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - funtion to print line diagonaly
+ *
+ * Return: return 0 always
+ * @n: n count the diagonal line
+ */
+void print_diagonal(int n)
+{
+	int j;
+	int i;
+
+	for (j = 1; j <= n; j++)
+	{
+		for (i = 1; i <= j; i++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+
+	}
+}

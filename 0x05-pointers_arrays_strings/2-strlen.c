0528ffdacdb15f8ee3c22a9116552a90cdd50b96
Egba James <ugwuayijameson@gmail.com>
2023-03-27 17:15:13 -0700

    count string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - check the string
+ * @s: count the string
+ * Return: returns a.
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++;)
+	{ 
+		a++;
+	}
+	return (a);
+}

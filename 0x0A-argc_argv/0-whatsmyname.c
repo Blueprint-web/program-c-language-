0cf9cd5938a63135a2160e35eca5bf4e1c425940
Jameson <ugwuayijameson@gmail.com>
2023-04-07 22:52:39 +0000

    the argument

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - the main function
+ * @argc: counter
+ * @argv: the programe output
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	int i;

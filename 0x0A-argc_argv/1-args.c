76ae91652c025f0cfd75625d3daf708472f3dbf7
Jameson <ugwuayijameson@gmail.com>
2023-04-11 20:22:12 +0000

    the argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,13 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int i = 0;
-	int j;
+	(void) argv;
 
-	for (i = 0; i < argc; i++)
-	{
-		j = *argv[i];
-	}
 	printf("%d\n", argc - 1);
-	return (j);
+	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!(isdigit(str[count])))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(int argc, char *argv[])
 {
 	int i;
 	int result = 0;
 
-	for (i = 0; i < argc; i++)
+	i = 1;
+	while (i < argc)
 	{
-		int x = strtol(argv[i], NULL, 10);
-		result = result + x;
+		if (check_num(argv[i]))
+		{
+			int x = atoi(argv[i]);
+
+			result = result + x;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		i++;
 	}
-	if ()
-	{
-		printf("Error");
-	} else
-		printf("%d\n", result);
-	return(0);
+	printf("%d\n", result);
+	return (0);
 }

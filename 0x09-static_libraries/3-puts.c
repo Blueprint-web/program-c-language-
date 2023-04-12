d11551e830b2af1e7f53ead3da6225582f1cb61c
Jameson <ugwuayijameson@gmail.com>
2023-04-12 02:18:46 +0000

    the static

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,4 +1,30 @@
 #include "main.h"
-void _puts(char *s)
+#include<stdio.h>
+/**
+ * _puts - does nothing
+ * @str: nothing
+ * Return: nothing
+ */
+void _puts(char *str)
 {
+	int i = 0;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
+
+/**
+ * main - Entry point
+ * _puts - does nothing
+ * Return: Always 0 (Success)
+ */
+
+int main(void) /*main - function for main entry*/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
